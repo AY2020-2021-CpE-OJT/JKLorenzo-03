537f924f03d607a6d1ce1a8633cc9bd7667919c0
Juruel Keanu Lorenzo <39931559+JKLorenzo@users.noreply.github.com>
2021-07-05 23:24:27 +0800

    add api manager to app

---
+++ phonebook/lib/managers/API.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+import 'package:http/http.dart';
+import '../structures/PBData.dart';
+
+final uri = Uri.http('10.0.2.2:3000', '/api/contacts');
+final Map<String, String> header = {
+  "Content-Type": "application/json",
+};
+
+class API {
+  static upsert(PBData data) async {
+    print('posting');
+    await post(uri, body: jsonEncode(data.toJSON()), headers: header);
+  }
+
+  static Future<List<PBData>> fetch() async {
+    print('getting');
+    Response response = await get(uri);
+    if (response.statusCode != 200) return [];
+    List<dynamic> body = jsonDecode(response.body);
+    List<PBData> data =
+        body.map((dynamic data) => PBData.fromJson(data)).toList();
+    return data;
+  }
+}
