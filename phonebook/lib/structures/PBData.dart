218750e040139934b8aea5056488fd871def564c
Juruel Keanu Lorenzo <39931559+JKLorenzo@users.noreply.github.com>
2021-07-05 23:24:11 +0800

    update PBData app structure

--- phonebook/lib/structures/PBData.dart
+++ phonebook/lib/structures/PBData.dart
@@ -5,8 +5,31 @@ class PBData {
 
   PBData(this.first_name, this.last_name, this.phone_numbers);
 
-  @override
-  String toString() {
-    return 'fn: $first_name \nln: $last_name \npn: ${phone_numbers.join(', ')}';
+  factory PBData.fromJson(Map<String, dynamic> json) {
+    List<dynamic> raw_phone_numbers = json['phone_numbers'];
+    List<String> phone_numbers = raw_phone_numbers
+        .map((phone_number) => phone_number.toString())
+        .toList();
+    return PBData(
+      json['first_name'] as String,
+      json['last_name'] as String,
+      phone_numbers,
+    );
+  }
+
+  equals(PBData data) {
+    if (this.first_name != data.first_name) return false;
+    if (this.last_name != data.last_name) return false;
+    if (this.phone_numbers.join('_') != data.phone_numbers.join('_'))
+      return false;
+    return true;
+  }
+
+  toJSON() {
+    return {
+      "first_name": this.first_name,
+      "last_name": this.last_name,
+      "phone_numbers": this.phone_numbers,
+    };
   }
 }
